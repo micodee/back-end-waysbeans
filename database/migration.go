38895fe4a6ccfce84d5ebd9c440ca08c28ab985f
micodee <martomhidayat>
2023-03-01 22:29:41 +0700

    CRUD users

--- database/migration.go
+++ database/migration.go
@@ -7,7 +7,10 @@ import (
 )
 
 func RunMigration() {
-	err := mysql.ConnDB.AutoMigrate(&models.Product{})
+	err := mysql.ConnDB.AutoMigrate(
+		&models.Product{},
+		&models.User{},
+	)
 
 	if err != nil {
 		fmt.Println(err)
